d09adbce0e0eb97cbaa698164d4f20f967d4b62c
Avery Morgan <avmorgan198@gmail.com>
2024-02-26 12:57:00 -0500

    adding functionality to all buttons

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,42 @@ const modalEditMessage = document.querySelector("#modal-edit-message");
 profileEditButton.addEventListener("click", function () {
   modalEditMessage.classList.add("modal_opened");
 });
+
+const modalCloseButton = document.querySelector(".modal__close-button");
+
+modalCloseButton.addEventListener("click", function () {
+  modalEditMessage.classList.remove("modal_opened");
+});
+
+const profileFormElement = document.querySelector(".modal__form");
+const nameInput = document.querySelector("#profile-name");
+const jobInput = document.querySelector("#about-me");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector("profile__description");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAboutMe = inputAboutMe.value;
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+/*const cardTemplate = document.querySelector("#card-template");*/
+
+function getCardElement(data) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  return cardElement;
+}
+
+const cardList = document.querySelector(".cards__list");
+
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardList.prepend(cardElement);
+});
