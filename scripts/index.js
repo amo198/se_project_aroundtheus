dd69a4316973cc323f7b1e3ce5aa865df1d1f28d
Avery Morgan <avmorgan198@gmail.com>
2024-02-28 22:35:18 -0500

    fixing submit button

--- scripts/index.js
+++ scripts/index.js
@@ -52,6 +52,11 @@ function handleProfileFormSubmit(evt) {
   profileDescription.textContent = profileDescriptionInput.value;
 }
 
+function closeProfileSubmit(evt) {
+  modalEditWindow.classList.remove("modal_opened");
+}
+
+modalEditWindow.addEventListener("submit", closeProfileSubmit);
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 
 function getCardElement(data) {
