f03cf31b048ef9d71af11d4b3d6df670d15fe5e6
Avery Morgan <avmorgan198@gmail.com>
2024-05-15 20:53:20 -0400

    Add: methods to section class

--- Components/Section.js
+++ Components/Section.js
@@ -1,6 +1,17 @@
 class Section {
   constructor({ objects, renderer }, cardSelector) {
-    renderItems();
-    addItem();
+    this._objects = objects;
+    this._renderer = renderer;
+    this._cardSelector = cardSelector;
+  }
+
+  addItem(element) {
+    this._cardSelector.append(element);
+  }
+
+  renderItems() {
+    this.renderItems.forEach((item) => {
+      renderer();
+    });
   }
 }
