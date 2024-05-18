86dbfa759918b882b904c7ac3e01a501a996cad7
Avery Morgan <avmorgan198@gmail.com>
2024-05-18 11:11:44 -0400

    Add: popup with image class

--- Components/PopupWithForm.js
+++ Components/PopupWithForm.js
@@ -1,5 +1,14 @@
 import Popup from "./Popup.js";
 
 class PopupWithFrom extends Popup {
-  constructor({ popupSelector, handleFormSubmit }) {}
+  constructor({ popupSelector, handleFormSubmit }) {
+    super({ popupSelector });
+    this._popupElement.querySelector(/*popup form*/);
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  close() {
+    this.popupForm.reset();
+    //super calls the parent class
+  }
 }
---
+++ Components/PopupWithImage.js
@@ -0,0 +1,11 @@
+import Popup from "./Popup.js";
+
+class PopupWithImage extends Popup {
+  constructor({name, link}) {
+    this.open(data) {
+        //set the image src and alt
+        //set the caption's textContent
+        super.open();
+    }
+  }
+}
