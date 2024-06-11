5cf51d4413001ea330d882ef12f240d6a8b03cf4
Avery Morgan <avmorgan198@gmail.com>
2024-06-11 19:57:26 -0400

    Fix: branch error

--- src/Api.js
+++ src/Api.js
@@ -5,22 +5,20 @@ class Api {
 
   getInitialCards() {
     return fetch("https://around-api.en.tripleten-services.com/v1", {
-        headers: {
-          authorization: "a5df8bb7-ccf7-4af1-a820-819df810a6c4"
-        }
-      })
-        .then(res => {
-          if (res.ok) {
-            return res.json();
-          }
-          
-          return Promise.reject(`Error: ${res.status}`);
-        });
-    }
-  }
+      headers: {
+        authorization: "a5df8bb7-ccf7-4af1-a820-819df810a6c4",
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
 
-  // other methods for working with the API
+      return Promise.reject(`Error: ${res.status}`);
+    });
+  }
 }
+
+// other methods for working with the API
 
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
